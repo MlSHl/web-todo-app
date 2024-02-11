8417e8ed42598b49dd337c10f70af0b455fc7c15
mishi <mishi.beraia@gmail.com>
2024-02-11 04:31:27 +0400

    Fixed some bugs regarding adding same item multiple times.

--- webApp.py
+++ webApp.py
@@ -2,21 +2,26 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
-st.title("My To-do App")
-st.subheader("This is my todo app.")
-st.write("This is to write lists.")
+st.title("Mo-do App")
 
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+    if st.session_state["new_todo"] != "":
+        todo = st.session_state["new_todo"] + "\n"
+        print(todo)
+        todos.append(todo)
+        functions.write_todos(todos)
+        st.session_state["new_todo"] = ""
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=f"{index} {todo}")
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[f"{index} {todo}"]
+        st.session_state["new_todo"] = ""
+        st.rerun()
 
 st.text_input(label=" ", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
-
-st.session_state
